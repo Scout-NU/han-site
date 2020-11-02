f68b25bd016d3e8dd8054bac6e970d0743b56d8e
wang-er <33679181+wang-er@users.noreply.github.com>
2020-11-02 01:03:04 -0500

    added vars for all query calls in contact

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -6,10 +6,46 @@ import Layout from '../components/layout'
 class ContactPage extends React.Component {
     render(){
         const siteTitle = get(this,  'props.data.site.siteMetadata.title')
+        const pageTitle = get(this,  'props.data.contentfulContactPage.headline')
+        const pagesubtitle = get(this,  'props.data.contentfulContactPage.tagline')
+        const emailTitle = get(this,  'props.data.contentfulContactPage.emailHeader')
+        const emailSubtitle = get(this,  'props.data.contentfulContactPage.emailDescription')
+        const emailIcon = get(this,  'props.data.contentfulContactPage.emailIcon.image.file.url')
+        const emailValue = get(this,  'props.data.contentfulContactPage.email.value.value')
+        const linkedInTitle = get(this,  'props.data.contentfulContactPage.linkedInHeader')
+        const linkedInSubtitle = get(this,  'props.data.contentfulContactPage.linkedInDescription')
+        const linkedInIcon = get(this,  'props.data.contentfulContactPage.linkedInIcon.image.file.url')
+        const linkedInValue = get(this,  'props.data.contentfulContactPage.linkedIn.value.value')
+        const newsletterTitle = get(this,  'props.data.contentfulContactPage.newsletterHeader')
+        const newsletterSubtitle = get(this,  'props.data.contentfulContactPage.newsletterDescription')
+        const newsletterIcon = get(this,  'props.data.contentfulContactPage.newsletterIcon.image.file.url')
+        const faqTitle = get(this,  'props.data.contentfulContactPage.faqHeader')
+        const faqIcon = get(this,  'props.data.contentfulContactPage.faqImage.file.url')
+        const socialMediaTitle = get(this,  'props.data.contentfulContactPage.socialMediaHeader')
+        const socialMediaSubtitle = get(this,  'props.data.contentfulContactPage.socialMediaDescription')
+       
 
         return (
             <Layout location={this.props.location}>
               <Helmet title={siteTitle} />
+                <div>{pageTitle}</div>
+                <div>{pagesubtitle}</div>
+                <div>{emailTitle}</div>
+                <div>{emailSubtitle}</div>
+                <img src={emailIcon}/>
+                <div>{emailValue}</div>
+                <div>{linkedInTitle}</div>
+                <div>{linkedInSubtitle}</div>
+                <img src={linkedInIcon}/>
+                <div>{linkedInValue}</div>
+                <div>{newsletterTitle}</div>
+                <div>{newsletterSubtitle}</div>
+                <img src={newsletterIcon}/>
+                <div>{faqTitle}</div>
+                <img src={faqIcon}/>
+                <div>{socialMediaTitle}</div>
+                <div>{socialMediaSubtitle}</div>
+
             </Layout>
         )
     }
