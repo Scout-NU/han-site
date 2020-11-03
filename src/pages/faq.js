65c4efbf0cd74177b69927ea7a277628fb9d34a7
wang-er <33679181+wang-er@users.noreply.github.com>
2020-11-03 17:02:30 -0500

    faq querying displays

--- src/pages/faq.js
+++ src/pages/faq.js
@@ -6,10 +6,21 @@ import Layout from '../components/layout'
 class FAQPage extends React.Component {
     render(){
         const siteTitle = get(this,  'props.data.site.siteMetadata.title')
+        const pageTitle = get(this,  'props.data.contentfulFaqPage.headline')
+        const category1Label = get(this,  'props.data.contentfulFaqPage.category1Label')
+        const category1Questions = get(this,  'props.data.contentfulFaqPage.category1Questions')
+        const category2Label = get(this,  'props.data.contentfulFaqPage.category2Label')
+        const category2Questions = get(this,  'props.data.contentfulFaqPage.category2Questions')
+        const category3Label = get(this,  'props.data.contentfulFaqPage.category3Label')
+        const category3Questions = get(this,  'props.data.contentfulFaqPage.category3Questions')
 
         return (
             <Layout location={this.props.location}>
               <Helmet title={siteTitle} />
+              <h1>{pageTitle}</h1>
+              <div>{category1Label}</div>
+              <div>{category2Label}</div>
+              <div>{category3Label}</div>
             </Layout>
         )
     }
