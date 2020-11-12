7a954e1f195423f96ebc5d91f2635e88f16fafef
angelinahan <han.ang@husky.neu.edu>
2020-11-11 21:56:56 -0500

    Changed layout.js to a function component and added static query for footer data. Added social icons

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,23 +1,29 @@
 import React from 'react'
-import { Link } from 'gatsby'
+import { Link, graphql } from 'gatsby'
+import Img from 'gatsby-image'
 import styles from './footer.module.css'
 import { StaticQuery } from 'gatsby'
+import InstagramIcon from "../../images/instagramIcon"
+import LinkedInIcon from "../../images/linkedInIcon"
+import MediumIcon from "../../images/mediumIcon"
 
-export default () => (
-    <StaticQuery query = { graphql`
-    query FooterQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `
-    }
-    render={ data => (
-        <div>
-            {data.site.siteMetadata.title}
-            I'm a cooler footer!
-        </div>
-    )} />
+
+const Footer = ({ contentfulFooter, contactInfo }) => (
+  <div>
+    <Link to={contactInfo.instagramLink}>
+      <InstagramIcon />
+    </Link>
+    <Link to={contactInfo.linkedInLink}>
+      <LinkedInIcon />
+    </Link>
+    <Link to={contactInfo.mediumLink}>
+      <MediumIcon />
+    </Link>
+
+    <Img alt="logo for Mosaic, with the word MOSAIC in all caps and " fluid={contentfulFooter.mosaicLogo.fluid} />
+    <Img alt="logo for D'Amore McKim" fluid={contentfulFooter.damoreMcKimLogo.fluid} />
+    <p>{contentfulFooter.descriptionAboutHan}</p>
+  </div>
 )
+
+export default Footer
