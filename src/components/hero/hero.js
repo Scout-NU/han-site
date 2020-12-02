ad02aa1de3eb37a4dd20433335225da320d5572a
angelinahan <han.ang@northeastern.edu>
2020-12-02 17:43:54 -0500

    Added gatsby-plugin-schema-snapshot to lock down our schema at this current time, cleaned up queries after updating contentful fields

--- src/components/hero/hero.js
+++ src/components/hero/hero.js
@@ -10,10 +10,11 @@ export default ({ data }) => (
       alt={data.name}
       fluid={data.heroImage.fluid}
     />
-    <div className={styles.heroDetails}>
+    <h1>{data.headline}</h1>
+    {/* <div className={styles.heroDetails}>
       <h3 className={styles.heroHeadline}>{data.name}</h3>
       <p className={styles.heroTitle}>{data.title}</p>
       <p>{data.shortBio.shortBio}</p>
-    </div>
+    </div> */}
   </div>
 )
