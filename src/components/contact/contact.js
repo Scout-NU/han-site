8701baac43a330fe39a21255eb1205a63b26e403
angelinahan <han.ang@northeastern.edu>
2020-12-23 00:38:47 -0500

    started contact page

---
+++ src/components/contact/contact.js
@@ -0,0 +1,69 @@
+import React from "react"
+import styled from "styled-components"
+import { BaseMarginContainer } from "../base/base-components"
+import { darkGray, lightGray, navy, teal } from "../base/colors"
+import { HANdescription, HANh1, HANsubh1, HANsubh4, HANbody } from "../base/fonts"
+
+const ContactHeadingContainer = styled.div`
+    margin: 200px 0px 70px 0px;
+`
+
+const FAQButton = styled.div`
+    background: ${teal};
+    width: 50%;
+    height: 200px;
+    position: absolute;
+    top: 0;
+    right: 0;
+`
+
+const ContactInfoContainer = styled.div`
+    position: relative;
+
+`
+
+const ContactGroup = styled.div`
+    margin-bottom: 70px;
+`
+
+const ContactHANdescription = styled(HANdescription)`
+    margin-bottom: 8px;
+`
+
+const LinkBlock = styled.a`
+`
+
+const Contact = ({ headline, tagline, emailHeader, emailDescription, linkedInHeader, linkedInDescription,
+    newsletterHeader, newsletterDescription, featuredNewsletter, socialMediaHeader, socialMediaDescription, contactInfoLinks }) => (
+    <BaseMarginContainer>
+        <ContactHeadingContainer>
+            <HANh1>{headline}</HANh1>
+            <HANsubh1 color={darkGray}>{tagline}</HANsubh1>
+        </ContactHeadingContainer>
+        <ContactInfoContainer>
+            <FAQButton />
+            <ContactGroup>
+                <ContactHANdescription color={navy}>{emailHeader}</ContactHANdescription>
+                <HANsubh4 color={darkGray}>{emailDescription}</HANsubh4>
+                <HANbody color={teal}>{contactInfoLinks.email}</HANbody>
+            </ContactGroup>
+            <ContactGroup>
+                <ContactHANdescription color={navy}>{linkedInHeader}</ContactHANdescription>
+                <HANsubh4 color={darkGray}>{linkedInDescription}</HANsubh4>
+                <HANbody color={teal}>{contactInfoLinks.linkedInLink}</HANbody>
+            </ContactGroup>
+            <ContactGroup>
+                <ContactHANdescription color={navy}>{newsletterHeader}</ContactHANdescription>
+                <HANsubh4 color={darkGray}>{newsletterDescription}</HANsubh4>
+                <HANbody as="a" color={teal} href={`https://${featuredNewsletter.newsletterFile.file.url}`}>{featuredNewsletter.title}</HANbody>
+            </ContactGroup>
+            <ContactGroup>
+                <ContactHANdescription color={navy}>{socialMediaHeader}</ContactHANdescription>
+                <HANsubh4 color={darkGray}>{socialMediaDescription}</HANsubh4>
+                <HANbody color={teal}>{contactInfoLinks.email}</HANbody>
+            </ContactGroup>
+        </ContactInfoContainer>
+    </BaseMarginContainer>
+)
+
+export default Contact;
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,33 +4,34 @@ import { Helmet } from 'react-helmet'
 import get from 'lodash/get'
 import Layout from '../components/layout'
 import Img from 'gatsby-image'
+import Contact from "../components/contact/contact"
+
 class ContactPage extends React.Component {
-    render(){
-        const siteTitle = get(this,  'props.data.site.siteMetadata.title')
-        const contact = get(this,  'props.data.contentfulContactPage')
-        return (
-            <Layout location={this.props.location}>
-              <Helmet title={siteTitle} />
-                <div>{contact.headline}</div>
-                <div>{contact.tagline}</div>
-                <div>{contact.emailHeader}</div>
-                <div>{contact.emailDescription}</div>
-                {/* <Img fluid={contact.emailIcon.image.fluid}/> */}
-                <div>{contact.email.value.value}</div>
-                <div>{contact.linkedInHeader}</div>
-                <div>{contact.linkedInDescription}</div>
-                {/* <Img fluid={contact.linkedInIcon.image.fluid}/> */}
-                <div>{contact.linkedIn.value.value}</div>
-                <div>{contact.newsletterHeader}</div>
-                <div>{contact.newsletterDescription}</div>
-                {/* <Img fluid={contact.newsletterIcon.image.fluid}/> */}
-                <div>{contact.faqHeader}</div>
-                {/* <Img fluid={contact.faqImage.fluid}/> */}
-                <div>{contact.socialMediaHeader}</div>
-                <div>{contact.socialMediaDescription}</div>
-            </Layout>
-        )
-    }
+  render() {
+    const siteTitle = get(this, 'props.data.site.siteMetadata.title')
+    const contact = get(this, 'props.data.contentfulContactPage')
+    const contactInfoLinks = get(this, 'props.data.contentfulContactInformation')
+    return (
+      <Layout location={this.props.location}>
+        <Contact
+          headline={contact.headline}
+          tagline={contact.tagline}
+          emailHeader={contact.emailHeader}
+          emailDescription={contact.emailDescription}
+          linkedInHeader={contact.linkedInHeader}
+          linkedInDescription={contact.linkedInDescription}
+          newsletterHeader={contact.newsletterHeader}
+          newsletterDescription={contact.newsletterDescription}
+          featuredNewsletter={contact.featuredNewsletter}
+          socialMediaHeader={contact.socialMediaHeader}
+          socialMediaDescription={contact.socialMediaDescription}
+          faqHeader={contact.faqHeader}
+          contactInfoLinks={contactInfoLinks}
+        ></Contact>
+        <Helmet title={siteTitle} />
+      </Layout>
+    )
+  }
 }
 
 export default ContactPage
@@ -47,23 +48,27 @@ export const pageQuery = graphql`
         tagline
         emailHeader
         emailDescription
-        email {
-          value {
-            value
-          }
-        }
         linkedInHeader
         linkedInDescription
-        linkedIn {
-          value {
-            value
-          }
-        }
         newsletterHeader
         newsletterDescription
+        featuredNewsletter {
+          title
+          newsletterFile {
+            file {
+              url
+            }
+          }
+        }
         faqHeader
         socialMediaHeader
         socialMediaDescription
       }
+    contentfulContactInformation {
+        email
+        instagramLink
+        linkedInLink
+        mediumLink
+    }
   }
 `
